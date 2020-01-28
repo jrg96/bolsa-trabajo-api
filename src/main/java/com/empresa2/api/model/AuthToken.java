c1f29b05aaab75f50b850f517e9649b34ba2a172
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-28 16:02:57 -0600

    Custom wrappers for JWT authentication

---
+++ src/main/java/com/empresa2/api/model/AuthToken.java
@@ -0,0 +1,33 @@
+package com.empresa2.api.model;
+
+/*
+ * Clase que sirve como contenedor para la informacion del token a regresar
+ * en el metodo /token/generate-token
+ * 
+ * Esta clase no mapea ninguna tabla de la DB
+ * */
+public class AuthToken 
+{
+
+    private String token;
+
+    public AuthToken()
+    {
+
+    }
+
+    public AuthToken(String token)
+    {
+        this.token = token;
+    }
+
+    public String getToken() 
+    {
+        return token;
+    }
+
+    public void setToken(String token) 
+    {
+        this.token = token;
+    }
+}
---
+++ src/main/java/com/empresa2/api/model/LoginUsuario.java
@@ -0,0 +1,35 @@
+package com.empresa2.api.model;
+
+/*
+ * Clase modelo ocupada como representacion de los datos (usuario y contraseña) enviados
+ * a traves de JSON en el POST request para autentificar
+ * 
+ * Esta clase no mapea ninguna tabla en la DB
+ * 
+ * */
+public class LoginUsuario 
+{
+	String username;
+	String password;
+	
+	
+	public String getUsername() 
+	{
+		return username;
+	}
+	
+	public void setUsername(String username) 
+	{
+		this.username = username;
+	}
+	
+	public String getPassword() 
+	{
+		return password;
+	}
+	
+	public void setPassword(String password) 
+	{
+		this.password = password;
+	}
+}
