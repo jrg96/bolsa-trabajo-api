e87342a5ac6ac79e2de1413fe727593f638704a3
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-28 15:46:39 -0600

    Base project added

---
+++ src/test/java/com/empresa2/api/BolsaTrabajoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa2.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaTrabajoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
