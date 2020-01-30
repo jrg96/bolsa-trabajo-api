33ef7aedffa4cef6d7af120a9682f1adcbaf4b16
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-29 19:05:22 -0600

    INSERT usuario, v1

--- src/main/java/com/empresa2/api/controller/UsuarioController.java
+++ src/main/java/com/empresa2/api/controller/UsuarioController.java
@@ -9,8 +9,11 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,8 @@ import com.empresa2.api.service.interf.IUsuarioService;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -40,8 +45,39 @@ public class UsuarioController
 	@Autowired
 	private MessageSource messageSource;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	
+	
+	/*
+	 * API endpoint para guardar un usuario
+	 * */
+	@PreAuthorize("hasAnyRole('ADMINISTRADOR')")
+	@PostMapping(value = "/")
+	public ResponseEntity<?> guardarUsuario(@Valid @RequestBody Usuario usuario)
+	{
+		/*
+		 * ---------------- ZONA DE DESPLIEGUE DE DATOS ---------------------------------------
+		 * */
+		// Encriptamos la password antes de guardar
+		usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+		this.usuarioService.guardarUsuario(usuario);
+		
+		
+		
+		// Devolvemos la propia entidad usuario, HATEOAS habilitado
+		UsuarioModel usuarioModel = this.usuarioModelAssembler.toModel(usuario);
+		usuarioModel.add(
+				linkTo(methodOn(UsuarioController.class).obtenerListaUsuarios(null)).withRel("list-usuarios")
+		);
+		
+		return ResponseEntity.ok(usuarioModel);
+	}
+	
+	/*
+	 * API Endpoint para obtener los datos de un usuario
+	 * */
 	@PreAuthorize("hasAnyRole('ADMINISTRADOR')")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<?> obtenerUsuario(@PathVariable("id") int id)
@@ -67,6 +103,11 @@ public class UsuarioController
 		return ResponseEntity.ok(usuarioModel);
 	}
 	
+	
+	
+	/*
+	 * API Endpoint para obtener la lista de usuarios, paginada
+	 * */
 	@PreAuthorize("hasAnyRole('ADMINISTRADOR')")
 	@GetMapping(value = "/list")
 	public ResponseEntity<?> obtenerListaUsuarios(Pageable pageable)
@@ -79,4 +120,6 @@ public class UsuarioController
 		
 		return ResponseEntity.ok(usuariosM);
 	}
+	
+	
 }
